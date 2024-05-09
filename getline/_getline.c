b51e5b04582e2aaf2690103084ab3a0c753959ff
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-05-09 00:02:01 -0500

    fix

--- getline/_getline.c
+++ getline/_getline.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define READ_SIZE 128
+
 char *_getline(int fd) {
     static char *static_buffer = NULL;
     static size_t buffer_capacity = 0;
@@ -13,6 +15,10 @@ char *_getline(int fd) {
     char *new_buffer = realloc(static_buffer, buffer_capacity);
     ssize_t bytes_read = read(fd, static_buffer + buffer_length, buffer_capacity - buffer_length);
 
+    if (fd < 0) {
+        return NULL;
+    }
+
     /*Initialize the static buffer if it's NULL*/
     if (static_buffer == NULL) {
         buffer_capacity = 128;
@@ -20,6 +26,7 @@ char *_getline(int fd) {
         if (static_buffer == NULL) {
             return NULL;
         }
+        memset(static_buffer, 0xFF, buffer_capacity);
     }
 
     while (1) {
@@ -33,18 +40,20 @@ char *_getline(int fd) {
             
             strncpy(line, static_buffer, line_len); 
             line[line_len] = '\0';
+
             memmove(static_buffer, newline_pos + 1, remaining_len); 
             buffer_length = remaining_len;
             return line;
         }
         
    
-        if (buffer_length == buffer_capacity) {
+        if (buffer_length >= buffer_capacity) {
             buffer_capacity *= 2;
             if (new_buffer == NULL) {
                 return NULL;
             }
             static_buffer = new_buffer;
+              memset(static_buffer + buffer_length, 0xFF, buffer_capacity - buffer_length);
         }
 
         if (bytes_read <= 0) {
