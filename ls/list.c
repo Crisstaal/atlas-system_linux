67af4806b24bf0570c56ee05d90c6d44242c7a6c
Crisstaal <muyungacrys@gmail.com>
2024-05-26 18:16:45 -0500

    fixing

--- ls/list.c
+++ ls/list.c
@@ -9,20 +9,29 @@
 void list_directory(const char *dir, int op_l, int op_A) {
     struct dirent *d;
     DIR *dh = opendir(dir);
-    int i;
+    int i, m, n;
     File *head = NULL, *tail = NULL;
     int file_count = 0;
     char path[512];
     File *f = head;
     File *new_file;
     size_t j, len;
-    File **file_array = malloc(file_count * sizeof(File *));
+    File **file_array = NULL;
     
     if (!dh) {
         fprintf(stderr, "hls: cannot open directory '%s': %s\n", dir, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
+     size_t dir_len = strlen(dir);
+    if (dir_len >= sizeof(path) - 2) {
+        fprintf(stderr, "Directory path too long\n");
+        exit(EXIT_FAILURE);
+    }
+
+    memcpy(path, dir, dir_len);
+    path[dir_len] = '/';
+    path[dir_len + 1] = '\0';
 
     while ((d = readdir(dh)) != NULL) {
         if ((!op_A && d->d_name[0] == '.') || (op_A && (strcmp(d->d_name, ".") == 0 || strcmp(d->d_name, "..") == 0))) continue;
@@ -33,18 +42,24 @@ void list_directory(const char *dir, int op_l, int op_A) {
             exit(EXIT_FAILURE);
         }
 
-        for (j = 0; d->d_name[j] != '\0' && j < sizeof(new_file->name) - 1; ++j) {
+        for (j = 0; j < sizeof(new_file->name) - 1; ++j) {
+            if (d->d_name[j] == '\0') {
+                new_file->name[j] = '\0';
+                break;
+            }
             new_file->name[j] = d->d_name[j];
         }
         new_file->name[j] = '\0';
 
-        len = strlen(dir);
-        strncpy(path, dir, sizeof(path) - 1);
-        path[len] = '/';
-        strncpy(path + len + 1, d->d_name, sizeof(path) - len - 1);
+        len = strlen(d->d_name);
+        if (dir_len + 1 + len >= sizeof(path)) {
+            fprintf(stderr, "Path length exceeds buffer size\n");
+            exit(EXIT_FAILURE);
+        }
+        memcpy(path + dir_len + 1, d->d_name, len + 1);
 
-        if (stat(path, &new_file->st) != 0) {
-            perror("stat");
+        if (lstat(path, &new_file->st) != 0) {
+            perror("lstat");
             exit(EXIT_FAILURE);
         }
 
@@ -54,26 +69,32 @@ void list_directory(const char *dir, int op_l, int op_A) {
         else tail->next = new_file;
         
         tail = new_file;
+        file_count++;
     }
     closedir(dh);
 
-    while (f != NULL) {
-        file_count++;
-        f = f->next;
-    }
-
+    file_array = malloc(file_count * sizeof(File *));
     if (file_array == NULL) {
         perror("malloc");
         exit(EXIT_FAILURE);
     }
     
     f = head;
-    for (i = 0; i < file_count; i++) {
-        file_array[i] = f;
+    i = 0;
+    while (f != NULL) {
+        file_array[i++] = f;
         f = f->next;
     }
 
-    qsort(file_array, file_count, sizeof(File *), compare_names);
+    for (m = 1; m < file_count; ++m) {
+        File *key = file_array[m];
+        n = m - 1;
+        while (n >= 0 && strcmp(file_array[n]->name, key->name) > 0) {
+            file_array[n + 1] = file_array[n];
+            n = n - 1;
+        }
+        file_array[n + 1] = key;
+    }
 
     for (i = 0; i < file_count; i++) {
         if (op_l) {
@@ -86,3 +107,4 @@ void list_directory(const char *dir, int op_l, int op_A) {
     free(file_array);
     free_file_list(head);
 }
+    
