e968e5f6327fe469026887a044a734207116a598
Crisstaal <muyungacrys@gmail.com>
2024-10-21 00:47:54 -0500

    fix

--- readelf/0-hreadelf.c
+++ readelf/0-hreadelf.c
@@ -1,10 +1,12 @@
-// 0-hreadelf.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
-#include "hreadelf.h"
+#include <gelf.h>
+#include <libelf.h>
+
+#define ELF_MAGIC_LEN 16
 
 /* Function to open the ELF file */
 int open_file(const char *filename) {
@@ -21,29 +23,60 @@ int check_elf(Elf *elf) {
     return (elf_kind(elf) == ELF_K_ELF) ? 1 : (fprintf(stderr, "The file is not an ELF file.\n"), 0);
 }
 
+/* Function to return a string for the OS/ABI value */
+const char* osabi_to_string(unsigned char osabi) {
+    switch (osabi) {
+        case ELFOSABI_SYSV: return "UNIX - System V";
+        case ELFOSABI_NETBSD: return "UNIX - NetBSD";
+        case ELFOSABI_LINUX: return "UNIX - Linux";
+        // Add other OS/ABI cases as needed
+        default: return "Unknown";
+    }
+}
+
+/* Function to return a string for the machine type */
+const char* machine_to_string(unsigned int machine) {
+    switch (machine) {
+        case EM_X86_64: return "Advanced Micro Devices X86-64";
+        case EM_386: return "Intel 80386";
+        case EM_SPARC: return "Sparc";
+        // Add other machine types as needed
+        default: return "Unknown";
+    }
+}
+
 /* Function to print the ELF header */
 void print_header(const GElf_Ehdr *ehdr) {
     printf("ELF Header:\n  Magic:   ");
-    for (int i = 0; i < EI_NIDENT; i++) {
+    for (int i = 0; i < ELF_MAGIC_LEN; i++) {
         printf("%02x ", ehdr->e_ident[i]);
     }
     printf("\n  Class:                             %s\n",
            (ehdr->e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32");
+    
     printf("  Data:                              %s\n",
-           (ehdr->e_ident[EI_DATA] == ELFDATA2LSB) ? "Little Endian" : "Big Endian");
-    printf("  Version:                           %d\n", ehdr->e_ident[EI_VERSION]);
-    printf("  OS/ABI:                            %d\n", ehdr->e_ident[EI_OSABI]);
+           (ehdr->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
+           
+    printf("  Version:                           %d (current)\n", ehdr->e_ident[EI_VERSION]);
+    printf("  OS/ABI:                            %s\n", osabi_to_string(ehdr->e_ident[EI_OSABI]));
     printf("  ABI Version:                       %d\n", ehdr->e_ident[EI_ABIVERSION]);
-    printf("  Type:                              0x%x\n", ehdr->e_type);
-    printf("  Machine:                           0x%x\n", ehdr->e_machine);
+    
+    printf("  Type:                              ", ehdr->e_type);
+    switch (ehdr->e_type) {
+        case ET_EXEC: printf("EXEC (Executable file)\n"); break;
+        // Add other types as necessary
+        default: printf("0x%x\n", ehdr->e_type); break;
+    }
+    
+    printf("  Machine:                           %s\n", machine_to_string(ehdr->e_machine));
     printf("  Entry point address:               0x%lx\n", (unsigned long)ehdr->e_entry);
-    printf("  Start of program headers:          %ld\n", (long)ehdr->e_phoff);
-    printf("  Start of section headers:          %ld\n", (long)ehdr->e_shoff);
+    printf("  Start of program headers:          %ld (bytes into file)\n", (long)ehdr->e_phoff);
+    printf("  Start of section headers:          %ld (bytes into file)\n", (long)ehdr->e_shoff);
     printf("  Flags:                             0x%x\n", ehdr->e_flags);
-    printf("  Size of this header:               %d\n", ehdr->e_ehsize);
-    printf("  Size of program headers:           %d\n", ehdr->e_phentsize);
+    printf("  Size of this header:               %d (bytes)\n", ehdr->e_ehsize);
+    printf("  Size of program headers:           %d (bytes)\n", ehdr->e_phentsize);
     printf("  Number of program headers:         %d\n", ehdr->e_phnum);
-    printf("  Size of section headers:           %d\n", ehdr->e_shentsize);
+    printf("  Size of section headers:           %d (bytes)\n", ehdr->e_shentsize);
     printf("  Number of section headers:         %d\n", ehdr->e_shnum);
     printf("  Section header string table index: %d\n", ehdr->e_shstrndx);
 }
