3591454d8fb95afbcc133855a217e27c46734369
Crisstaal <muyungacrys@gmail.com>
2024-10-21 00:52:25 -0500

    fix

--- readelf/0-hreadelf.c
+++ readelf/0-hreadelf.c
@@ -47,27 +47,40 @@ const char* machine_to_string(unsigned int machine) {
 
 /* Function to print the ELF header */
 void print_header(const GElf_Ehdr *ehdr) {
-    printf("ELF Header:\n  Magic:   ");
+    printf("ELF Header:\n");
+    printf("  Magic:                             ");
     for (int i = 0; i < ELF_MAGIC_LEN; i++) {
         printf("%02x ", ehdr->e_ident[i]);
     }
-    printf("\n  Class:                             %s\n",
+    printf("\n");
+    
+    printf("  Class:                             %s\n", 
            (ehdr->e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32");
     
-    printf("  Data:                              %s\n",
+    printf("  Data:                              %s\n", 
            (ehdr->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
-           
+    
     printf("  Version:                           %d (current)\n", ehdr->e_ident[EI_VERSION]);
     printf("  OS/ABI:                            %s\n", osabi_to_string(ehdr->e_ident[EI_OSABI]));
     printf("  ABI Version:                       %d\n", ehdr->e_ident[EI_ABIVERSION]);
-    
-    printf("  Type:                              ", ehdr->e_type);
+
+    // Updated printf for Type
+    printf("  Type:                              ");
     switch (ehdr->e_type) {
-        case ET_EXEC: printf("EXEC (Executable file)\n"); break;
-        // Add other types as necessary
-        default: printf("0x%x\n", ehdr->e_type); break;
+        case ET_EXEC: 
+            printf("EXEC (Executable file)\n");
+            break;
+        case ET_DYN:
+            printf("DYN (Shared object file)\n");
+            break;
+        case ET_REL:
+            printf("REL (Relocatable file)\n");
+            break;
+        default:
+            printf("0x%x\n", ehdr->e_type);
+            break;
     }
-    
+
     printf("  Machine:                           %s\n", machine_to_string(ehdr->e_machine));
     printf("  Entry point address:               0x%lx\n", (unsigned long)ehdr->e_entry);
     printf("  Start of program headers:          %ld (bytes into file)\n", (long)ehdr->e_phoff);
@@ -80,7 +93,6 @@ void print_header(const GElf_Ehdr *ehdr) {
     printf("  Number of section headers:         %d\n", ehdr->e_shnum);
     printf("  Section header string table index: %d\n", ehdr->e_shstrndx);
 }
-
 void print_elf_header(const char *filename) {
     int fd;
     Elf *elf;
