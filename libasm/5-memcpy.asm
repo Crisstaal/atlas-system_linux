f9dbe2537bdd91863afcb022161ff1283ef21e79
Crisstaal <muyungacrys@gmail.com>
2024-10-31 22:42:46 -0500

    fix 5

--- libasm/5-memcpy.asm
+++ libasm/5-memcpy.asm
@@ -3,21 +3,19 @@ BITS 64
 section .text
 global asm_memcpy
 
-section .text
-global asm_memcpy
-
 ; void *asm_memcpy(void *dest, const void *src, size_t n);
 asm_memcpy:
     ; Check if n is zero
     test rdx, rdx            ; Check if n is zero
-    jz .done                 ; If n is zero, just return dest
+    jz .done                 ; If n is zero, return dest
 
-    ; Move src and dest to registers
+    ; Initialize pointers
     mov rax, rdi             ; rax = dest
     mov rcx, rsi             ; rcx = src
 
 .copy_loop:
-    movsb                    ; Copy byte from src (rcx) to dest (rax)
+    mov al, [rcx]            ; Load byte from src into al
+    mov [rax], al            ; Store byte from al into dest
     inc rax                  ; Increment dest pointer
     inc rcx                  ; Increment src pointer
     dec rdx                  ; Decrement n
