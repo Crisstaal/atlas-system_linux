425f3b55dc2e489a443e846bc8f2a5d05cc69030
Crisstaal <muyungacrys@gmail.com>
2024-10-31 22:15:48 -0500

    fix

--- libasm/3-strchr.asm
+++ libasm/3-strchr.asm
@@ -4,30 +4,29 @@ section .text
     global asm_strchr        ; Make the function globally accessible
 
 asm_strchr:
-    ; Setting up the stack frame
-    push    rbp               ; Save the base pointer
-    mov     rbp, rsp          ; Establish the stack frame
-
-    mov     rdi, rdi          ; The pointer to the string (s)
-    mov     al, dil           ; Move the character (c) into al
+    ; Input: rdi = pointer to string (const char *s), rsi = character (int c)
+    
+    ; Check if the string is NULL
+    test rdi, rdi
+    jz .not_found       ; If NULL, go to not_found
 
 .loop:
-    cmp     byte [rdi], 0     ; Compare the current character to null terminator
-    je      .not_found        ; If it's null, we didn't find the character
-    cmp     byte [rdi], al    ; Compare the current character to the target character
-    je      .found            ; If they are equal, jump to found
-    inc     rdi               ; Move to the next character in the string
-    jmp     .loop             ; Repeat the loop
+    ; Load the current character from the string
+    mov al, [rdi]      ; Get the character at the current position
+    cmp al, 0          ; Check if it's the end of the string
+    je .not_found       ; If it is, go to not_found
+    
+    cmp al, sil        ; Compare with the character we are searching for
+    je .found          ; If found, jump to found
+    
+    ; Move to the next character
+    inc rdi            ; Increment pointer to the next character
+    jmp .loop          ; Repeat the loop
 
 .found:
-    mov     rax, rdi          ; Set the return value to the address of the found character
-    jmp     .done             ; Jump to done
+    mov rax, rdi       ; Return the pointer to the found character
+    ret
 
 .not_found:
-    xor     rax, rax          ; Return NULL (0) if not found
-
-.done:
-    ; Teardown the stack frame
-    mov     rsp, rbp          ; Restore the stack pointer
-    pop     rbp               ; Restore the base pointer
-    ret                       ; Return from the function
+    xor rax, rax       ; Return NULL (0) if not found
+    ret
