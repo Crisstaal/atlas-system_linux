ac80844d4bedc89c73bda046f1734e6bc7db4be6
Crisstaal <muyungacrys@gmail.com>
2024-10-31 23:26:26 -0500

    update

--- libasm/0-strlen.asm
+++ libasm/0-strlen.asm
@@ -2,7 +2,7 @@ BITS 64
 
     global my_strlen  ; Make the function accessible to other files
     section .text
-asm_strlen:
+my_strlen:
     ; Prototype: size_t my_strlen(const char *str);
 
     xor     rcx, rcx          ; Zero out rcx (this will be our counter)
--- libasm/7-puts.asm
+++ libasm/7-puts.asm
@@ -2,21 +2,24 @@ BITS 64
 
 section .text
 global asm_puts
+extern my_strlen       ; Import my_strlen from 0-strlen.asm
 
+; Prototype: size_t asm_puts(const char *str);
 asm_puts:
     ; Input: RDI - pointer to the string (const char *str)
-    ; The return value will be in RAX (size_t)
 
-    call my_strlen          ; Call the length function (my_strlen)
-    mov rbx, rax           ; Store the length in RBX for later use
-    
-    ; Prepare for the syscall
-    mov rax, 1        ; syscall number for sys_write
-    mov rdi, 1        ; file descriptor: stdout
-    mov rsi, RDI      ; rsi will point to the string
-    mov rdx, rBx      ; length of the string
+    call my_strlen            ; Call the length function (my_strlen)
+    mov rbx, rax              ; Store the length in RBX for later use
 
-    syscall            ; Invoke syscall to write the string
+    ; Set up for syscall to write
+    mov rax, 1                ; Syscall number for sys_write (1)
+    mov rdi, 1                ; File descriptor 1 is stdout
+    mov rsi, RDI              ; Pointer to the string (input from RDI)
+    mov rdx, rbx              ; Length of the string (in RBX)
 
-    mov rax, rbx           ; Return value is the length of the string
-    ret                ; Return from function
+    ; Perform the syscall
+    syscall                    ; Invoke the syscall (write)
+
+    ; Return the number of bytes written
+    mov rax, rbx              ; Return value is the length of the string
+    ret                        ; Return to the caller
