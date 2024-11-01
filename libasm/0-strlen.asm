275ae125f94809964dc019bf78de734d50696ae0
Crisstaal <muyungacrys@gmail.com>
2024-10-31 21:01:01 -0500

    solve

--- libasm/0-strlen.asm
+++ libasm/0-strlen.asm
@@ -1,9 +1,8 @@
 BITS 64
 
-section .text
-    global my_strlen  ; Make the function accessible to other files
-
-my_strlen:
+    global asm_strlen  ; Make the function accessible to other files
+    section .text
+asm_strlen:
     ; Prototype: size_t my_strlen(const char *str);
     
     xor     rcx, rcx          ; Zero out rcx (this will be our counter)
