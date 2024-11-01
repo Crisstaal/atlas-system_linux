5d3e092fe62723313a5edbf62954e022645a2582
Crisstaal <muyungacrys@gmail.com>
2024-10-31 23:47:15 -0500

    solve

--- libasm/12-strpbrk.asm
+++ libasm/12-strpbrk.asm
@@ -18,17 +18,19 @@ asm_strpbrk:  ; Label for the function
     test    al, al               ; Check if null (end of string)
     jz      .not_found           ; If null, character not found
 
-    ; Check if the character is in the accept string
-    push    rsi                  ; Save accept string pointer
-    call    .is_char_in_accept   ; Call helper function
-    pop     rsi                  ; Restore accept string pointer
+    ; Initialize j counter for accept
+    xor     rcx, rcx            ; rcx will serve as index for accept
 
-    test    al, al               ; Check if character was found in accept
-    jz      .not_found           ; If not found, continue
+.accept_loop:
+    mov     bl, byte [rsi + rcx] ; Load character from accept
+    test    bl, bl               ; Check if null (end of accept)
+    jz      .not_found           ; If null, character not found in accept
 
-    ; If found, return pointer to the character in s
-    lea     rax, [rdi + rax]     ; Load address of found character into rax
-    jmp     .done                ; Go to done
+    cmp     al, bl               ; Compare character from s with accept character
+    je      .found               ; If equal, character is found
+
+    inc     rcx                  ; Move to the next character in accept
+    jmp     .accept_loop         ; Repeat the check
 
 .not_found:
     inc     rax                  ; Increment s counter
@@ -39,26 +41,3 @@ asm_strpbrk:  ; Label for the function
     mov     rsp, rbp            ; Restore previous stack pointer
     pop     rbp                 ; Restore previous base pointer
     ret                         ; Return from the function
-
-; Helper function: Check if the character in al is in the accept string
-.is_char_in_accept:
-    xor     rcx, rcx            ; j counter
-
-.check_loop:
-    mov     bl, byte [rsi + rcx] ; Load character from accept
-    test    bl, bl               ; Check if null (end of accept)
-    jz      .not_found_accept     ; If null, character not found
-
-    cmp     al, bl               ; Compare character from s with accept character
-    je      .found_accept         ; If equal, character is found
-
-    inc     rcx                  ; Move to the next character in accept
-    jmp     .check_loop          ; Repeat the check
-
-.not_found_accept:
-    xor     al, al               ; Set return value to 0 (false)
-    ret
-
-.found_accept:
-    mov     al, 1                ; Set return value to 1 (true)
-    ret
