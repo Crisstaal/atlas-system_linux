9e88d16efa90e14d7447a558ba03c2bea7707364
Crisstaal <muyungacrys@gmail.com>
2024-11-01 00:08:36 -0500

    solve

--- libasm/11-strcspn.asm
+++ libasm/11-strcspn.asm
@@ -7,10 +7,6 @@ section .text
     global asm_strcspn    ; Make the function globally accessible
 
 asm_strcspn:  ; Label for the function
-    ; Setting up the stack frame
-    push    rbp                 ; Preserve base pointer on the stack
-    mov     rbp, rsp            ; Set the base pointer to the current stack pointer
-
     xor     rax, rax            ; Initialize counter (i = 0)
 
 .s_loop:
@@ -19,25 +15,7 @@ asm_strcspn:  ; Label for the function
     jz      .done                ; If null, we're done
 
     ; Check if the character is in the reject string
-    push    rsi                  ; Save reject string pointer
-    call    .is_char_in_reject   ; Call helper function
-    pop     rsi                  ; Restore reject string pointer
-
-    test    al, al               ; Check if character was found in reject
-    jnz     .done                ; If found, we're done
-
-    inc     rax                  ; Increment the count 
-    jmp     .s_loop              ; Repeat the loop
-
-.done:
-    ; Tearing down the stack frame
-    mov     rsp, rbp            ; Restore previous stack pointer
-    pop     rbp                 ; Restore previous base pointer
-    ret                         ; Return from the function
-
-; Helper function: Check if the character in al is in the reject string
-.is_char_in_reject:
-    xor     rcx, rcx            ; j counter
+    xor     rcx, rcx            ; Reset index for reject string
 
 .check_loop:
     mov     bl, byte [rsi + rcx] ; Load character from reject
@@ -45,15 +23,14 @@ asm_strcspn:  ; Label for the function
     jz      .not_found           ; If null, character not found
 
     cmp     al, bl               ; Compare character from s with reject character
-    je      .found               ; If equal, character is found
+    je      .done                ; If equal, character is found
 
     inc     rcx                  ; Move to the next character in reject
     jmp     .check_loop          ; Repeat the check
 
 .not_found:
-    xor     al, al               ; Set return value to 0 (false)
-    ret
+    inc     rax                  ; Increment the count
+    jmp     .s_loop              ; Go back to the main loop
 
-.found:
-    mov     al, 1                ; Set return value to 1 (true)
-    ret
+.done:
+    ret                          ; Return with the count in rax
