bbf2824b89818210b3268568210f74b2b5217215
Crisstaal <muyungacrys@gmail.com>
2024-10-31 22:53:59 -0500

    oops

--- libasm/6-putc.asm
+++ libasm/6-putc.asm
@@ -1,22 +1,20 @@
-BITS 64
-
-section .bss
-    buffer resb 1            ; Reserve 1 byte for the character to print
-
 section .text
 global asm_putc
 
 asm_putc:
-    ; Save the character from rdi
-    mov [buffer], dil        ; Store the character in the buffer
+    ; Reserve a buffer directly in the text section
+    ; Prepare a static buffer for writing
+    sub rsp, 8              ; Create space on the stack
+    mov byte [rsp], dil     ; Store the character in the stack
 
     ; Prepare for the syscall
-    mov rax, 1               ; syscall number for sys_write
-    mov rdi, 1               ; file descriptor: stdout
-    lea rsi, [buffer]        ; pointer to the buffer
-    mov rdx, 1               ; number of bytes to write
+    mov rax, 1              ; syscall number for sys_write
+    mov rdi, 1              ; file descriptor: stdout
+    lea rsi, [rsp]          ; pointer to the character
+    mov rdx, 1              ; number of bytes to write
 
-    syscall                   ; Invoke syscall to write the character
+    syscall                  ; Invoke syscall to write the character
 
-    mov rax, 1               ; Return the number of bytes written
-    ret                       ; Return from function
+    add rsp, 8              ; Clean up the stack
+    mov rax, 1              ; Return the number of bytes written
+    ret                      ; Return from function
