73614c4a75053b9180a8b353ec5608382169229c
Crisstaal <muyungacrys@gmail.com>
2024-10-31 22:02:33 -0500

    fix

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -10,31 +10,27 @@ asm_strncmp:
     ; rsi = s2 (second string)
     ; rdx = n (number of characters to compare)
 
-    ; Setting up the stack frame
     push    rbp              ; Save the base pointer
     mov     rbp, rsp         ; Establish the stack frame
 
-    ; Initialize loop counter
-    xor     rax, rax         ; Clear rax for the return value (default 0)
+    xor     rax, rax         ; Clear rax for the loop counter
 
 .loop:
-    ; Check if we have compared 'n' characters
     cmp     rax, rdx         ; Compare loop counter with n
     jge     .done             ; If we've reached n, exit the loop
 
-    ; Load the next byte of s1 and s2 into al and bl
     mov     al, [rdi + rax]   ; Load next byte from s1
     mov     bl, [rsi + rax]   ; Load next byte from s2
 
-    ; Compare the bytes
+    ; Check for null terminator in either string
+    test    al, al            ; Check if the character from s1 is null
+    jz      .equal            ; If s1 is null, treat as equal if s2 is also null
+    test    bl, bl            ; Check if the character from s2 is null
+    jz      .not_equal        ; If s2 is null, s1 is greater if al is not null
+
     cmp     al, bl           ; Compare the bytes
     jne     .not_equal       ; If they are not equal, jump to not_equal
 
-    ; Check for null terminator
-    test    al, al           ; Check if we reached the end of the string
-    jz      .equal           ; If al is 0, strings are equal so far
-
-    ; Move to the next character
     inc     rax              ; Increment the loop counter
     jmp     .loop            ; Repeat the loop
 
