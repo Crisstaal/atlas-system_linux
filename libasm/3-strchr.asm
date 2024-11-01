6c92c38eaa4639e85a9837609636f3e3621cb1aa
Crisstaal <muyungacrys@gmail.com>
2024-10-31 22:18:01 -0500

    fix 3

--- libasm/3-strchr.asm
+++ libasm/3-strchr.asm
@@ -1,20 +1,19 @@
 BITS 64
 
 section .text
-    global asm_strchr        ; Make the function globally accessible
+global asm_strchr        ; Make the function globally accessible
 
 asm_strchr:
     ; Input: rdi = pointer to string (const char *s), rsi = character (int c)
-    
+
     ; Check if the string is NULL
     test rdi, rdi
     jz .not_found       ; If NULL, go to not_found
 
 .loop:
-    ; Load the current character from the string
-    mov al, [rdi]      ; Get the character at the current position
+    mov al, [rdi]      ; Load the current character from the string
     cmp al, 0          ; Check if it's the end of the string
-    je .not_found       ; If it is, go to not_found
+    je .found           ; If it's the null terminator, return it
     
     cmp al, sil        ; Compare with the character we are searching for
     je .found          ; If found, jump to found
