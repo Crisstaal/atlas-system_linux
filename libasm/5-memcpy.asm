57b9fdd080fc93440fdc03aac225a9d0993ee980
Crisstaal <muyungacrys@gmail.com>
2024-10-31 22:39:10 -0500

    fix 5

--- libasm/5-memcpy.asm
+++ libasm/5-memcpy.asm
@@ -1,32 +1,27 @@
 BITS 64
 
 section .text
-    global asm_memcpy
+global asm_memcpy
 
+section .text
+global asm_memcpy
+
+; void *asm_memcpy(void *dest, const void *src, size_t n);
 asm_memcpy:
-    ; Prototypes: void *asm_memcpy(void *dest, const void *src, size_t n);
-    
-    push    rbp             ; Save the base pointer
-    mov     rbp, rsp        ; Set the base pointer to the current stack pointer
+    ; Check if n is zero
+    test rdx, rdx            ; Check if n is zero
+    jz .done                 ; If n is zero, just return dest
 
-    ; Check if n is zero, if yes, return dest
-    test    rdx, rdx        ; Check if n (size) is 0
-    jz      .done           ; If zero, return dest
+    ; Move src and dest to registers
+    mov rax, rdi             ; rax = dest
+    mov rcx, rsi             ; rcx = src
 
-    ; Move src and dest to registers for ease of use
-    mov     rax, rdi        ; Move dest to rax
-    mov     rcx, rsi        ; Move src to rcx
-
-    ; Copy n bytes from src to dest
 .copy_loop:
-    mov     al, [rcx]       ; Load byte from src into al
-    mov     [rax], al       ; Store byte into dest
-    inc     rax             ; Move to the next byte in dest
-    inc     rcx             ; Move to the next byte in src
-    dec     rdx             ; Decrement the count
-    jnz     .copy_loop      ; Repeat until n bytes are copied
+    movsb                    ; Copy byte from src (rcx) to dest (rax)
+    inc rax                  ; Increment dest pointer
+    inc rcx                  ; Increment src pointer
+    dec rdx                  ; Decrement n
+    jnz .copy_loop           ; Repeat until n is zero
 
 .done:
-    mov     rsp, rbp        ; Restore stack pointer
-    pop     rbp             ; Restore base pointer
-    ret                     ; Return dest pointer
+    ret                      ; Return dest
