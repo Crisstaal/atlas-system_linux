ec07056859e1d4cbbe539aa62b5e5df3ad81030f
Crisstaal <muyungacrys@gmail.com>
2024-11-13 22:53:50 -0600

    oops

--- nm_objdump/hnm.h
+++ nm_objdump/hnm.h
@@ -3,9 +3,11 @@
 
 #include <elf.h>
 #include <stdint.h>
+#include <elf.h>
 
-
-
+void print_symbol(const char *name, Elf64_Addr addr, char type);
+char get_symbol_type(GElf_Sym *sym, GElf_Shdr *shdr);
+void process_symbols(Elf *elf);
 
 
 #endif
