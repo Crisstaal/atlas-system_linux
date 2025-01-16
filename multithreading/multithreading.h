3ab70f82761bb159d2a58a0340d869826ad22c75
Crisstaal <muyungacrys@gmail.com>
2025-01-15 20:53:10 -0600

    save

--- multithreading/multithreading.h
+++ multithreading/multithreading.h
@@ -72,7 +72,7 @@ int tprintf(char const *format, ...);
 void blur_portion(blur_portion_t const *portion);
 void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel);
 int tprintf(char const *format, ...);
-list_t *prime_factors(char const *s);
-task_t *create_task(task_entry_t entry, void *param);
+/*list_t *prime_factors(char const *s);
+task_t *create_task(task_entry_t entry, void *param);*/
 
 #endif
