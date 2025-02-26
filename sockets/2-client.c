389e81005684b7e587e20dc47b4d6d3e2739cc2b
Crisstaal <muyungacrys@gmail.com>
2025-02-25 18:57:36 -0600

    fix

--- sockets/2-client.c
+++ sockets/2-client.c
@@ -9,53 +9,53 @@
 
 int main(int argc, char *argv[])
 {
-    int sock_fd, port;
-    struct sockaddr_in server_addr;
+	int sock_fd, port;
+	struct sockaddr_in server_addr;
 
-    /* Validate arguments */
-    if (argc != 3)
-    {
-        fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+	/* Validate arguments */
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-    /* Convert port number */
-    port = atoi(argv[2]);
+	/* Convert port number */
+	port = atoi(argv[2]);
 
-    /* Create socket */
-    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock_fd == -1)
-    {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
+	/* Create socket */
+	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock_fd == -1)
+	{
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
 
-    /* Configure server address struct */
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
+	/* Configure server address struct */
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
 
-    /* Convert and set IP address */
-    if (inet_pton(AF_INET, argv[1], &server_addr.sin_addr) <= 0)
-    {
-        perror("inet_pton");
-        close(sock_fd);
-        exit(EXIT_FAILURE);
-    }
+	/* Convert and set IP address */
+	if (inet_pton(AF_INET, argv[1], &server_addr.sin_addr) <= 0)
+	{
+		perror("inet_pton");
+		close(sock_fd);
+		exit(EXIT_FAILURE);
+	}
 
-    /* Connect to server */
-    if (connect(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
-    {
-        perror("connect");
-        close(sock_fd);
-        exit(EXIT_FAILURE);
-    }
+	/* Connect to server */
+	if (connect(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+	{
+		perror("connect");
+		close(sock_fd);
+		exit(EXIT_FAILURE);
+	}
 
-    /* Print success message */
-    printf("Connected to %s:%d\n", argv[1], port);
+	/* Print success message */
+	printf("Connected to %s:%d\n", argv[1], port);
 
-    /* Close socket */
-    close(sock_fd);
+	/* Close socket */
+	close(sock_fd);
 
-    return (0);
+	return (0);
 }
--- sockets/todo_api_0.c
+++ sockets/todo_api_0.c
@@ -10,92 +10,92 @@
 
 void handle_client(int client_fd, struct sockaddr_in client_addr)
 {
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_received;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_received;
 
-    /* Receive HTTP request */
-    bytes_received = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
-    if (bytes_received <= 0)
-    {
-        perror("recv");
-        close(client_fd);
-        return;
-    }
+	/* Receive HTTP request */
+	bytes_received = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
+	if (bytes_received <= 0)
+	{
+		perror("recv");
+		close(client_fd);
+		return;
+	}
 
-    buffer[bytes_received] = '\0'; /* Null-terminate */
+	buffer[bytes_received] = '\0'; /* Null-terminate */
 
-    /* Print client IP */
-    printf("Client connected: %s\n", inet_ntoa(client_addr.sin_addr));
+	/* Print client IP */
+	printf("Client connected: %s\n", inet_ntoa(client_addr.sin_addr));
 
-    /* Print raw HTTP request */
-    printf("Raw request: \"%s\"\n", buffer);
+	/* Print raw HTTP request */
+	printf("Raw request: \"%s\"\n", buffer);
 
-    /* Extract and print the first line (method, path, version) */
-    char method[16], path[256], version[16];
-    if (sscanf(buffer, "%15s %255s %15s", method, path, version) == 3)
-    {
-        printf("Method: %s\n", method);
-        printf("Path: %s\n", path);
-        printf("Version: %s\n", version);
-    }
+	/* Extract and print the first line (method, path, version) */
+	char method[16], path[256], version[16];
+	if (sscanf(buffer, "%15s %255s %15s", method, path, version) == 3)
+	{
+		printf("Method: %s\n", method);
+		printf("Path: %s\n", path);
+		printf("Version: %s\n", version);
+	}
 
-    /* Send HTTP 200 OK response */
-    send(client_fd, RESPONSE, strlen(RESPONSE), 0);
+	/* Send HTTP 200 OK response */
+	send(client_fd, RESPONSE, strlen(RESPONSE), 0);
 
-    /* Close connection */
-    close(client_fd);
+	/* Close connection */
+	close(client_fd);
 }
 
 int main(void)
 {
-    int server_fd, client_fd;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof(client_addr);
+	int server_fd, client_fd;
+	struct sockaddr_in server_addr, client_addr;
+	socklen_t client_len = sizeof(client_addr);
 
-    /* Create socket */
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1)
-    {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
+	/* Create socket */
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1)
+	{
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
 
-    /* Configure server */
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+	/* Configure server */
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    /* Bind */
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
-    {
-        perror("bind");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
+	/* Bind */
+	if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+	{
+		perror("bind");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
 
-    /* Listen */
-    if (listen(server_fd, 5) == -1)
-    {
-        perror("listen");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
+	/* Listen */
+	if (listen(server_fd, 5) == -1)
+	{
+		perror("listen");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
 
-    printf("Server listening on port %d\n", PORT);
+	printf("Server listening on port %d\n", PORT);
 
-    /* Accept clients in a loop */
-    while (1)
-    {
-        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
-        if (client_fd == -1)
-        {
-            perror("accept");
-            continue;
-        }
-        handle_client(client_fd, client_addr);
-    }
+	/* Accept clients in a loop */
+	while (1)
+	{
+		client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+		if (client_fd == -1)
+		{
+			perror("accept");
+			continue;
+		}
+		handle_client(client_fd, client_addr);
+	}
 
-    /* Close server socket */
-    close(server_fd);
-    return 0;
+	/* Close server socket */
+	close(server_fd);
+	return 0;
 }
