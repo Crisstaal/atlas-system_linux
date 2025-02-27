a4aac08fe0f90b118b790d69ec213471ff952650
Crisstaal <muyungacrys@gmail.com>
2025-02-26 22:15:55 -0600

    trying

--- sockets/0-server.c
+++ sockets/0-server.c
@@ -7,50 +7,88 @@
 #include <unistd.h>
 
 #define SYSCALL_ERROR -1
+#define PORT 12345
+#define BACKLOG 8
 
+/**
+ * initialize_server - Initializes the server address struct.
+ * @server_addr: Pointer to the server address struct.
+ */
+void initialize_server(struct sockaddr_in *server_addr)
+{
+    server_addr->sin_family = AF_INET;
+    server_addr->sin_port = htons(PORT);
+    server_addr->sin_addr.s_addr = htonl(INADDR_ANY);
+}
+
+/**
+ * create_socket - Creates a socket.
+ * Return: File descriptor for the socket.
+ */
+int create_socket(void)
+{
+    int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_fd == SYSCALL_ERROR)
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    return sock_fd;
+}
+
+/**
+ * bind_socket - Binds the socket to the server address.
+ * @sock_fd: File descriptor of the socket.
+ * @server_addr: Pointer to the server address struct.
+ */
+void bind_socket(int sock_fd, struct sockaddr_in *server_addr)
+{
+    if (bind(sock_fd, (struct sockaddr *)server_addr, sizeof(*server_addr))
+        == SYSCALL_ERROR)
+    {
+        perror("bind");
+        close(sock_fd);
+        exit(EXIT_FAILURE);
+    }
+}
+
+/**
+ * listen_socket - Puts the socket in listening mode.
+ * @sock_fd: File descriptor of the socket.
+ */
+void listen_socket(int sock_fd)
+{
+    if (listen(sock_fd, BACKLOG) == SYSCALL_ERROR)
+    {
+        perror("listen");
+        close(sock_fd);
+        exit(EXIT_FAILURE);
+    }
+}
+
+/**
+ * main - Entry point for the program.
+ * Return: 0 on success.
+ */
 int main(void)
 {
-	int sock_fd, bind_result, listen_result;
-	struct sockaddr_in server_addr = {
-	    .sin_family = AF_INET,
-	    .sin_port = htons(12345),
-	    .sin_addr.s_addr = htonl(INADDR_ANY)};
+    int sock_fd;
+    struct sockaddr_in server_addr;
 
-	/* Request socket from the kernel */
-	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock_fd == SYSCALL_ERROR)
-	{
-		perror("socket");
-		exit(EXIT_FAILURE);
-	}
+    initialize_server(&server_addr);
+    sock_fd = create_socket();
+    bind_socket(sock_fd, &server_addr);
+    listen_socket(sock_fd);
 
-	/* Bind socket to server address 0.0.0.0:12345 */
-	bind_result = bind(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-	if (bind_result == SYSCALL_ERROR)
-	{
-		perror("bind");
-		close(sock_fd);
-		exit(EXIT_FAILURE);
-	}
+    printf("Server listening on port %d\n", PORT);
 
-	/* Transition socket to listening state */
-	listen_result = listen(sock_fd, 8);
-	if (listen_result == SYSCALL_ERROR)
-	{
-		perror("listen");
-		close(sock_fd);
-		exit(EXIT_FAILURE);
-	}
+    /* Hang indefinitely */
+    while (1)
+    {
+        pause(); /* Wait for signals */
+    }
 
-	printf("Server listening on port 12345\n");
-
-	/* Hang indefinitely */
-	while (1)
-	{
-		pause(); /* Wait for signals */
-	}
-
-	/* Close the socket (though this won't be reached) */
-	close(sock_fd);
-	return (0);
+    /* Close the socket (though this won't be reached) */
+    close(sock_fd);
+    return (0);
 }
